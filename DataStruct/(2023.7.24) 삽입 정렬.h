dc8b000ac06b91eac76c93e8a135cc08ecd0668e
ddoong426 <130463147+ddoong426@users.noreply.github.com>
2023-07-25 21:43:28 +0900

    (2023.7.25) 트리

---
+++ DataStruct/(2023.7.24) 삽입 정렬.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 두 번째 자료부터 시작하여 그 앞(왼쪽)의
+	// 자료들과 비교하여 삽입할 위치를 지정한 후
+	// 자료를 뒤로 옮기고 지정한 자리에 자료를 삽입하여
+	// 정렬하는 알고리즘입니다.
+
+	// 시간 복잡도 O(n^2)
+#pragma endregion
+
+	return 0;
+}
